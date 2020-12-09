7b52b1f25696a26a5b61db4ed26a9ffff78dc64c
cristian18-code <cristiangarzon180@gmail.com>
2020-12-09 16:48:49 -0500

    ..

--- principal.php
+++ principal.php
@@ -6,8 +6,8 @@
 <head>
 <title>Página de Inicio</title>
 <!-- Estilos css -->
-<link href="styles/index.css" rel="stylesheet" type="text/css"/>
 <link href="styles/reset.css" rel="stylesheet" type="text/css"/>
+<link rel="stylesheet" href="styles/header.css">
 <!-- Estilos css -->
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
@@ -16,28 +16,9 @@
 <!--Fuentes de google-->
 </head>
 <body>
-<!--header start here-->
-<h1>Página de Inicio</h1>
-<div class="header agile">
-	<div class="wrap">
-		<div class="login-main wthree">
-			<div class="login">
-			<h3>Bienvenid@ al sistema  <i><?php echo $login_session; ?></i></h3>
+<!--header Inicia Aqui-->
+<?php include 'sistema/header.php';?>
+	
 
-			<div class="clear"> </div>
-				<h4><a href="config/logout.php"> Cerrar sesión</a></h4>
-			</div>
-			
-			
-		</div>
-	</div>
-</div>
-<!--header end here-->
-<!--copy rights end here-->
-<div class="copy-rights w3l">		 	
-	<p>© <?php echo date("Y");?> MedContactCenter </p>
-</div>
-<!--copyrights start here-->
- 
 </body>
 </html>
