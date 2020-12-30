d7ccad2506b4446d87ee7f14b6b95eb9e8c97d16
cristian18-code <cristiangarzon180@gmail.com>
2020-12-30 18:07:48 -0500

    ..

--- sistema/nav.php
+++ sistema/nav.php
@@ -5,20 +5,20 @@
   <li class="principal"> <a href="#"> <span class="icon-user-tie"></span> Administrador </a>
     <ul>
       <li><a href="./reportar_incidencia.php"> <span class="icon-wrench"> </span> Reportar </a></li>
-      <li><a href="listado.php">  <span class="icon-eye"> </span> Seguimiento </a></li>
-      <li><a href="listado.php">  <span class="icon-paste"> </span> Informes </a></li>
-      <li><a href="listado.php">  <span class="icon-users"> </span> Usuarios &nbsp;&nbsp; <span style="font-size: 14px;" class="icon-circle-right"></span> </a>
+      <li><a href="#">  <span class="icon-eye"> </span> Seguimiento </a></li>
+      <li><a href="#">  <span class="icon-paste"> </span> Informes </a></li>
+      <li><a href="#">  <span class="icon-users"> </span> Usuarios &nbsp;&nbsp; <span style="font-size: 14px;" class="icon-circle-right"></span> </a>
         <ul>
           <li><a href=".\crear_usuario.php">  <span class="icon-user-plus"> </span> Crear usuario </a>
-          <li><a href="listado.php">  <span class="icon-clipboard"> </span> Lista de usuarios </a>
+          <li><a href="#">  <span class="icon-clipboard"> </span> Lista de usuarios </a>
         </ul>
       </li>
     </ul>
   </li>
     <li><a href="#"> <span class="icon-opsgenie"> </span> Supervisor </a>
       <ul>
-        <li><a href="registro_u.php"> <span class="icon-wrench"> </span> Reportar </a></li>
-        <li><a href="listado.php">  <span class="icon-eye"> </span> Seguimiento </a></li>
+        <li><a href="./reportar_incidencia.php"> <span class="icon-wrench"> </span> Reportar </a></li>
+        <li><a href="#">  <span class="icon-eye"> </span> Seguimiento </a></li>
       </ul>
     </li>
   </ul>
